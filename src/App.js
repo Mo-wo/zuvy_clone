dffae90958a9e9ac2e93372846c03648343f0f39
Mowo <toryvick20@gmail.com>
2022-08-01 23:31:22 +0100

    Minor adjustments

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './Home';
 // import Section1 from './Section1';
 // import Section2 from './Section2'
 // import Section3 from './Section3';
-import Section4 from './Section4';
+// import Section4 from './Section4';
 import Section5 from './Section5';
 import Section6 from './Section6';
 import Section7 from './Section7';
