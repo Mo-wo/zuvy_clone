0baad09589eb7acea0e7cccf3a1633cc88e992c1
Mowo <toryvick20@gmail.com>
2022-08-14 23:16:53 +0100

    Update Section4.js

--- src/Section4.js
+++ src/Section4.js
@@ -5,11 +5,11 @@ const Section4 = () => {
     return ( 
         <section className={styles.section}>
             <div className={styles.container}>
-                <div className={styles.text}>
-                    <h2 className={styles.h2}>Features</h2>
-                    <p>Zuvy gives you the capital and flexibility<br/>
-                    that you need to grow your business and cover your expenses</p>
-                </div>
+//                 <div className={styles.text}>
+//                     <h2 className={styles.h2}>Features</h2>
+//                     <p>Zuvy gives you the capital and flexibility<br/>
+//                     that you need to grow your business and cover your expenses</p>
+//                 </div>
                 <div className={styles.imageWrapper}>
                     <img className={styles.image} src={features} alt="Screenshot of features" />
                 </div>
