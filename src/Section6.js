0beb698e5c83a81e3c4532674e2d79384ea58445
Mowo <toryvick20@gmail.com>
2022-08-15 23:42:46 +0100

    Update Section6.js

--- src/Section6.js
+++ src/Section6.js
@@ -4,25 +4,26 @@ import WaitlistButton from './Waitlist.js';
 import plane from './images/spaceshuttle1.png';
 
 
+
 const Section6 = () => {
     return ( 
         <section className={styles.section}>
-            <div className={styles.container}>
-                <div className={styles.content}>
-                    <h2 className={styles.h2}>Be one of the <br/> first Users</h2>
-                    <p className={styles.p}>Set your own payment terms on invoices that you<br/> recieve from vendors</p>
-                    <form className={styles.subscribe}>
-                        <input className={styles.input} type="email" 
-                        name="email" placeholder="Email Address"/>
-                        <div className={styles.buttonContainer}>
-                            <WaitlistButton /> 
-                        </div>
-                    </form>
-                </div>
-                <div className={styles.spacecraft}>
-                    <img className={styles.plane} src={plane} alt="Space Shuttle Illustration"/>
-                </div>
-            </div>
+//             <div className={styles.container}>
+//                 <div className={styles.content}>
+//                     <h2 className={styles.h2}>Be one of the <br/> first Users</h2>
+//                     <p className={styles.p}>Set your own payment terms on invoices that you<br/> recieve from vendors</p>
+//                     <form className={styles.subscribe}>
+//                         <input className={styles.input} type="email" 
+//                         name="email" placeholder="Email Address"/>
+//                         <div className={styles.buttonContainer}>
+//                             <WaitlistButton /> 
+//                         </div>
+//                     </form>
+//                 </div>
+//                 <div className={styles.spacecraft}>
+//                     <img className={styles.plane} src={plane} alt="Space Shuttle Illustration"/>
+//                 </div>
+//             </div>
 
         </section>
      );
